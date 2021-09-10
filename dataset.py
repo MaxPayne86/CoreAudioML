16c72d3097aab15614a7ab7a51611e3f5b66c2f2
Massimo Pennazio <maxipenna@libero.it>
2021-09-10 16:29:59 +0200

    Added support for float32

--- dataset.py
+++ dataset.py
@@ -10,6 +10,8 @@ import os
 def audio_converter(audio):
     if audio.dtype == 'int16':
         return audio.astype(np.float32, order='C') / 32768.0
+    elif audio.dtype == 'float32':
+        return audio.astype(np.float32, order='C')
     else:
         print('unimplemented audio data type conversion...')
 
