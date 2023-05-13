589de49cd117c956e8f52fe22900c9707b40b966
Massimo Pennazio <maxipenna@libero.it>
2023-05-13 15:41:50 +0200

    Using auraloss own FIR implementation, more reliable in the long term

--- training.py
+++ training.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from auraloss.perceptual import FIRFilter
+
 # ESR loss calculates the Error-to-signal between the output/target
 class ESRLoss(nn.Module):
     def __init__(self):
@@ -41,43 +43,22 @@ class DCLoss(nn.Module):
         return loss
 
 
-# PreEmph is a class that applies an FIR pre-emphasis filter to the signal, the filter coefficients are in the
-# filter_cfs argument, and lp is a flag that also applies a low pass filter
-# Only supported for single-channel!
 class PreEmph(nn.Module):
-    def __init__(self, filter_cfs, low_pass=0):
+    def __init__(self, filter_type='hp', coef=0.85, fs=48000):
         super(PreEmph, self).__init__()
-        self.epsilon = 0.00001
-        self.zPad = len(filter_cfs) - 1
-
-        self.conv_filter = nn.Conv1d(1, 1, 2, bias=False)
-        self.conv_filter.weight.data = torch.tensor([[filter_cfs]], requires_grad=False)
-
-        self.low_pass = low_pass
-        if self.low_pass:
-            self.lp_filter = nn.Conv1d(1, 1, 2, bias=False)
-            self.lp_filter.weight.data = torch.tensor([[[0.85, 1]]], requires_grad=False)
+        self.preemph = FIRFilter(filter_type=filter_type, coef=coef, fs=fs)
 
     def forward(self, output, target):
-        # zero pad the input/target so the filtered signal is the same length
-        output = torch.cat((torch.zeros(self.zPad, output.shape[1], 1), output))
-        target = torch.cat((torch.zeros(self.zPad, target.shape[1], 1), target))
-        # Apply pre-emph filter, permute because the dimension order is different for RNNs and Convs in pytorch...
-        output = self.conv_filter(output.permute(1, 2, 0))
-        target = self.conv_filter(target.permute(1, 2, 0))
-
-        if self.low_pass:
-            output = self.lp_filter(output)
-            target = self.lp_filter(target)
-
+        output, target = self.preemph(output.permute(1, 2, 0), target.permute(1, 2, 0))
         return output.permute(2, 0, 1), target.permute(2, 0, 1)
+
 
 class LossWrapper(nn.Module):
     def __init__(self, losses, pre_filt=None):
         super(LossWrapper, self).__init__()
         loss_dict = {'ESR': ESRLoss(), 'DC': DCLoss()}
         if pre_filt:
-            pre_filt = PreEmph(pre_filt)
+            pre_filt = PreEmph(filter_type=pre_filt, fs=48000)
             loss_dict['ESRPre'] = lambda output, target: loss_dict['ESR'].forward(*pre_filt(output, target))
         loss_functions = [[loss_dict[key], value] for key, value in losses.items()]
 
